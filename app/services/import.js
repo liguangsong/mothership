21e71de038ceaa09c8b44ad1940679d61bef400a
Yanan Guo <gynsolomon@gmail.com>
2014-03-22 14:18:51 +0800

    [yanan] for merge

--- app/services/import.js
+++ app/services/import.js
@@ -3,12 +3,6 @@ var Track = mongoose.model('Track');
 var schedule = require('node-schedule');
 var tracks = require('../controllers/tracks');
 
-var users = require('../controllers/users');
-var fs = require('fs');
-var file ='xiangmu.json';
-var data;
-
-
 
 exports.start = function () {
     schedule.scheduleJob('0 * * * *', function () {
@@ -25,23 +19,6 @@ exports.start = function () {
                         console.log(err.message + JSON.stringify(track));
                     }
                 });
-            });
-        });
-        var json = fs.readFile(file, 'utf8', function (err, data) {
-            if (err) {
-                console.log('Error: ' + err);
-                return;
-            }
-
-            data = JSON.parse(data);
-            data.forEach(function(json){
-                users.createUser(json);
-                /*User.find({"username":json.username},function (err, results) {
-                 if(err) {
-                 console.log('mongoose find error:' + JSON.stringify(err) +"the user is"+json.username);
-                 return;
-                 }
-                 });*/
             });
         });
     });
